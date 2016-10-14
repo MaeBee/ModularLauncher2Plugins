fd3de07e1ade10bf326554ca0dc81dc97729bb59
gobbo1008 <technofreak91@web.de>
2016-10-14 15:16:09 +0200

    Initial Commit
    Basic structure for Launch plugin laid down, RSS plugin as dummy to
    standardise project structure.

---
+++ ModularLauncher2Launch/Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using ModularLauncherUtil;
+
+namespace ModularLauncher2Launch
+{
+    public class Plugin : IPlugin
+    {
+        IPluginHost Host = null;
+
+        string Name = "Launch";
+        int ID = 0;
+        string Description = "Plugin for mod management.";
+        string Author = "Jannik \"gobbo\" Balkau";
+        string Version = "0.1";
+
+        System.Windows.Forms.UserControl MainInterface = new MainInterface();
+        System.Windows.Forms.UserControl SettingsPanel = new SettingsPanel();
+
+        event EventHandler SendNotification;
+        event EventHandler LaunchGame;
+
+        void Initialize();
+        void Dispose();
+    }
+}
---
+++ ModularLauncher2RSS/Plugin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModularLauncher2RSS
+{
+    public class Plugin
+    {
+    }
+}
